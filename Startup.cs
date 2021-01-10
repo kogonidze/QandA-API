1c4c558f3088ff7fb8481d8b5e37f8f0b45c868d
kogonidze <kottok1998@gmail.com>
2021-01-10 21:00:52 +0300

    added cors policy

--- Startup.cs
+++ Startup.cs
@@ -46,12 +46,20 @@ namespace QandA
 
             services.AddScoped<IDataRepository, DataRepository>();
 
+            services.AddCors(options =>
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials()
+                    .WithOrigins("http://localhost:3000")));
+
             services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("CorsPolicy");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
